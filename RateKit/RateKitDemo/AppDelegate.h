ae9c8d908c829b1c21fd909e02df2b639ee0cfc5
HsiangHo <ho_cheung@qq.com>
2018-11-03 12:51:41 +0800

    init project

---
+++ RateKit/RateKitDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RateKitDemo
+//
+//  Created by Jovi on 11/3/18.
+//  Copyright © 2018 Jovi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ RateKit/RateKitDemo/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  RateKitDemo
+//
+//  Created by Jovi on 11/3/18.
+//  Copyright © 2018 Jovi. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
