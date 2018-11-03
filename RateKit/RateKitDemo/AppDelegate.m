bacd0b172b3186f2d1140995900c50db074bb73e
HsiangHo <ho_cheung@qq.com>
2018-11-03 19:40:56 +0800

    added demo and some resources

--- RateKit/RateKitDemo/AppDelegate.m
+++ RateKit/RateKitDemo/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <RateKit/RateKit.h>
 
 @interface AppDelegate ()
 
@@ -17,8 +18,21 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    RateConfigure *configure = [[RateConfigure alloc] init];
+    [configure setName:@"Love Ratekit?"];
+    [configure setIcon:[NSImage imageNamed:@"demo-icon"]];
+    [configure setDetailText:@"We look forward to your Star and Pull Request to make Ratekit better and better : )\n⭐️⭐️⭐️⭐️⭐️"];
+    [configure setLikeButtonTitle:@"Star Now!"];
+    [configure setIgnoreButtonTitle:@"Maybe later"];
+    [configure setRateURL:[NSURL URLWithString:@"https://github.com/HsiangHo/RateKit"]];
+    
+    static RateWindowController *rateWindowController = nil;
+    rateWindowController = [[RateWindowController alloc] initWithConfigure:configure];
+    [rateWindowController setRateTimeout:10];
+    [rateWindowController requestRateWindow:RateWindowPositionTopRight withRateCompletionCallback:^(RateResult rlst) {
+        NSLog(@"%lu",(unsigned long)rlst);
+    }];
 }
-
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
