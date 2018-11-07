9bdf059d618a3f907be85030b32b67cfa6f26b57
HsiangHo <ho_cheung@qq.com>
2018-11-07 23:49:59 +0800

    compatible with 10.14

--- RateKit/RateKit/RateWindowController.m
+++ RateKit/RateKit/RateWindowController.m
@@ -120,6 +120,7 @@
     [_lbName setBezeled:NO];
     [_lbName setSelectable:NO];
     [_lbName setTextColor:[NSColor colorWithCalibratedRed:80/255.0 green:80/255.0 blue:80/255.0 alpha:1.0]];
+    [_lbName setBackgroundColor:[NSColor clearColor]];
     [_lbName setFont:[NSFont fontWithName:@"HelveticaNeue" size:21]];
     [_lbName setAlignment:NSCenterTextAlignment];
     [_lbName setStringValue:[_configure name]];
@@ -130,6 +131,7 @@
     [_lbDetailText setBezeled:NO];
     [_lbDetailText setSelectable:NO];
     [_lbDetailText setTextColor:[NSColor grayColor]];
+    [_lbDetailText setBackgroundColor:[NSColor clearColor]];
     [_lbDetailText setFont:[NSFont fontWithName:@"HelveticaNeue" size:12]];
     [_lbDetailText setLineBreakMode:NSLineBreakByWordWrapping];
 //    [_lbDetailText setAlignment:NSCenterTextAlignment];
